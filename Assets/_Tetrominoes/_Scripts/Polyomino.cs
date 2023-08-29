d231d89c39a51db95625dd171ac3d58a125dbfe6
TheKuroEver <mariusztang@outlook.com>
2023-08-29 14:01:08 +0100

    Move position contiguity check to vector extensions

--- Assets/_Tetrominoes/_Scripts/Polyomino.cs
+++ Assets/_Tetrominoes/_Scripts/Polyomino.cs
@@ -11,24 +11,10 @@ public class Polyomino
             throw new System.InvalidOperationException("No positions provided to Polyomino constructor.");
         if (positions.Length != positions.Distinct().Count())
             throw new System.InvalidOperationException($"Duplicate positions ({positions.ToDebugString()}) were provided to the Polyomino constructor.");
-        if (!AreContiguousPositions(positions))
+        if (!positions.AreContiguousPositions())
             throw new System.InvalidOperationException($"The positions provided to the Polyomino constructor ({positions.ToDebugString()}) are not all connected.");
         _relativeOccupiedPositions = positions.ToArray();
         SnapToOrigin();
-    }
-
-    private bool AreContiguousPositions(Vector2Int[] positions) {
-        var connectedSection = new List<Vector2Int> { positions[0] };
-        var remainingPositions = positions.Skip(1).ToList();
-        List<Vector2Int> newPositions;
-
-        do {
-            newPositions = remainingPositions.Where(pos => connectedSection.Any(secPos => pos.IsAdjacentTo(secPos))).ToList();
-            connectedSection.AddRange(newPositions);
-            remainingPositions = remainingPositions.Except(newPositions).ToList();
-        } while (newPositions.Any());
-
-        return connectedSection.Count == positions.Length;
     }
 
     private void SnapToOrigin() {
--- Assets/_Tetrominoes/_Scripts/VectorExtensions.cs
+++ Assets/_Tetrominoes/_Scripts/VectorExtensions.cs
@@ -9,4 +9,18 @@ public static class VectorExtensions
     public static string ToDebugString(this IEnumerable<Vector2Int> vectors) => vectors.Select(v => v.ToDebugString()).Join(", ");
 
     public static bool IsAdjacentTo(this Vector2Int lhs, Vector2Int rhs) => Mathf.Abs(lhs.x - rhs.x) + Mathf.Abs(lhs.y - rhs.y) == 1;
+
+    public static bool AreContiguousPositions(this IEnumerable<Vector2Int> positions) {
+        var connectedSection = new List<Vector2Int> { positions.First() };
+        var remainingPositions = positions.Skip(1).ToList();
+        List<Vector2Int> newPositions;
+
+        do {
+            newPositions = remainingPositions.Where(pos => connectedSection.Any(secPos => pos.IsAdjacentTo(secPos))).ToList();
+            connectedSection.AddRange(newPositions);
+            remainingPositions = remainingPositions.Except(newPositions).ToList();
+        } while (newPositions.Any());
+
+        return connectedSection.Count == positions.ToArray().Length;
+    }
 }
