070340366b059cbdb1b391595353f4f57962a5c5
TheKuroEver <mariusztang@outlook.com>
2023-09-01 12:29:35 +0100

    Add polyomino renderer and basic instantiate method

---
+++ Assets/_Tetrominoes/_Scripts/PolyominoRenderers/MonominoRenderer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class MonominoRenderer : MonoBehaviour
+{
+    private MeshRenderer _renderer;
+
+    public Color Colour {
+        get { return _renderer.material.color; }
+        set { _renderer.material.color = value; }
+    }
+    
+#pragma warning disable IDE0051
+    private void Awake() => _renderer = GetComponent<MeshRenderer>();
+}
---
+++ Assets/_Tetrominoes/_Scripts/PolyominoRenderers/PolyominoRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolyominoRenderer : MonoBehaviour
+{
+    private MonominoRenderer[] _monominoes;
+
+    public static PolyominoRenderer Instantiate(Transform parent, Vector2Int position, Polyomino polyomino, MonominoRenderer monominoPrefab) {
+        var renderer = new GameObject("Polyomino").AddComponent<PolyominoRenderer>();
+        renderer.transform.parent = parent;
+        renderer.transform.localScale = Vector3.one;
+        renderer.transform.localPosition = new Vector3(position.x, 0, position.y);
+
+        foreach (Vector2Int pos in polyomino.GetPositionsFromOrigin()) {
+            var monomino = Instantiate(monominoPrefab, renderer.transform);
+            monomino.transform.localPosition = new Vector3(pos.x, 0, pos.y);
+        }
+
+        return renderer;
+    }
+}
